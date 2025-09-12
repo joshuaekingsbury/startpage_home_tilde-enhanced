a497de1be7206d9644cfe1c381fe2afc69e8eddd
joshuaekingsbury <6468162+joshuaekingsbury@users.noreply.github.com>
2025-09-12 19:27:42 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
       name: 'Google',
       key: '*',
       url: 'https://google.com',
-      search: '/search?udm=14&q='
+      search: '/search?q=%s&udm=14'
     },
     {
       name: 'Duckduckgo',
